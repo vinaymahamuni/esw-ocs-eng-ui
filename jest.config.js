d2527607b32601c314293e3785dbc622f1209f86
ManishGowardipe <gowardipemanish95@gmail.com>
2021-02-15 12:26:28 +0530

    trying esw-ts to compile in jest

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,17 @@
+const esModules = ['@tmtsoftware/esw-ts']
 module.exports = {
   roots: ['<rootDir>/src', '<rootDir>/test'],
   preset: 'ts-jest',
   testRegex: ['(/(test)/.*|(\\.|/)(test|spec))\\.(ts|tsx)?$'],
   testPathIgnorePatterns: ['test/utils/*'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'node'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleDirectories: ['node_modules', 'src', 'test'],
+  transformIgnorePatterns: [`/node_modules/(?!${esModules})`],
   verbose: true,
+  moduleNameMapper: {
+    '\\.(css|sass|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/test/__mocks__/fileMocks.ts'
+  },
   coverageThreshold: {
     global: {
       branches: 80,
